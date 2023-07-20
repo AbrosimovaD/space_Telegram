e2e9c3ca9b112adc44e80104e6535869fe747cc3
AbrosimovaD <114830550+AbrosimovaD@users.noreply.github.com>
2023-07-20 15:27:45 +0300

    переименование функций

--- files_from_url_info_and_load.py
+++ files_from_url_info_and_load.py
@@ -4,7 +4,7 @@ from os import path
 from urllib import parse
 
 
-def image_load(url, path, filename):
+def download_image(url, path, filename):
     Path(path).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
@@ -12,7 +12,7 @@ def image_load(url, path, filename):
         file.write(response.content)
 
 
-def file_info(url):
+def get_file_name_and_type(url):
     file_path = parse.urlsplit(url).path
     file_name = parse.unquote(path.split(file_path)[1])
     file_type = path.splitext(file_name)[1]
