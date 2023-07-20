56d34d2e3da22fd307553a1e8699c628fb2c3e4d
AbrosimovaD <114830550+AbrosimovaD@users.noreply.github.com>
2023-07-20 15:24:56 +0300

    переименование переменной bot_api на bot_token

--- publish_to_telegram.py
+++ publish_to_telegram.py
@@ -1,5 +1,5 @@
 import telegram
 
-def publish_to_telegram(bot_api, chat_id,photo_url):
-    bot = telegram.Bot(token=bot_api)
+def publish_to_telegram(bot_token, chat_id,photo_url):
+    bot = telegram.Bot(token=bot_token)
     bot.send_photo(chat_id=chat_id, photo=photo_url)
