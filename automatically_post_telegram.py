0526c9909c3cbc2095510ca7ad100065da8e0375
AbrosimovaD <114830550+AbrosimovaD@users.noreply.github.com>
2023-07-20 15:25:37 +0300

    переименование переменной bot_api на bot_token

--- automatically_post_telegram.py
+++ automatically_post_telegram.py
@@ -6,14 +6,14 @@ import random
 from dotenv import load_dotenv
 
 
-def automatically_post_telegram(bot_api, chat_id, folder_to_post, frequency):
+def automatically_post_telegram(bot_token, chat_id, folder_to_post, frequency):
     images=[]
     for address, dirs, files in os.walk(folder_to_post):
         for name in files:
             images.append(os.path.join(address, name))
     while True:
         for image in images:
-            publish_to_telegram(bot_api, chat_id, open(image, 'rb'))
+            publish_to_telegram(bot_token, chat_id, open(image, 'rb'))
             time.sleep(int(frequency)*3600)
         random.shuffle(images)
 
@@ -23,10 +23,10 @@ def main():
     parser.add_argument('-f', '--folder_to_post', type=str, default = 'images', help='Path to load images')    
     folder_to_post = parser.parse_args().folder_to_post
     load_dotenv()
-    bot_api = os.environ['TELEGRAM_BOT_KEY']
+    bot_token = os.environ['TELEGRAM_BOT_KEY']
     chat_id = os.environ['TELEGRAM_CHAT_ID']
     frequency = os.environ['TELEGRAM_PUBLICATION_FREQUENCY']
-    automatically_post_telegram(bot_api, chat_id, folder_to_post, frequency)
+    automatically_post_telegram(bot_token, chat_id, folder_to_post, frequency)
 
 
 if __name__ == '__main__':
